199aa0ecb8703f0d631f4b5b7ba840125cd7a0a6
Pavelsiba <105110809+Pavelsiba@users.noreply.github.com>
2023-06-29 23:16:57 +0700

    add checks 1

--- config/storybook/preview.tsx
+++ config/storybook/preview.tsx
@@ -1,4 +1,5 @@
 import type { Preview } from '@storybook/react'
+import 'loki/configure-react'
 import 'app/styles/index.scss'
 import { ThemeDecorator } from '../../src/shared/config/storybook/themeDecorator/ThemeDecorator'
 import { Theme } from '../../src/app/providers/ThemeProvider'
