29c3c367bd6d08dac65b2287a20714aff6f33a7b
Pavelsiba <105110809+Pavelsiba@users.noreply.github.com>
2023-07-06 14:39:20 +0700

    fix 7

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import { ThemeProvider } from 'app/providers/ThemeProvider'
 import { render } from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import './shared/config/i18n/i18n'
-import App from './app/App'
+import App from './app/app'
 import { ErrorBoundary } from 'app/providers/ErrorBoundary'
 
 render(
