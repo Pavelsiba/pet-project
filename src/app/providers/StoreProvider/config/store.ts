e8acddabd6e0b114a770cf4c4b7157ce716a6a93
Pavelsiba <105110809+Pavelsiba@users.noreply.github.com>
2024-01-04 22:50:01 +0700

    update profile, add avatar

--- src/app/providers/StoreProvider/config/store.ts
+++ src/app/providers/StoreProvider/config/store.ts
@@ -1,14 +1,25 @@
-import { configureStore, type ReducersMapObject } from '@reduxjs/toolkit'
+import {
+  type CombinedState,
+  configureStore,
+  type ReducersMapObject,
+  type Reducer,
+  type Action
+} from '@reduxjs/toolkit'
 import { counterReducer } from 'entities/Counter'
 import { userReducer } from 'entities/User'
-import { type StateSchema } from './StateSchema'
+import { $api } from 'shared/api/api'
 import { createReducerManager } from './reducerManager'
+import { type ThunkExtraArg, type StateSchema } from './StateSchema'
 
 export type AppDispatch = ReturnType<typeof createReduxStore>['dispatch']
 
+const extraArg: ThunkExtraArg = {
+  api: $api
+}
+
 export function createReduxStore (
   initialState?: StateSchema,
-  asyncReducers?: ReducersMapObject<StateSchema>
+  asyncReducers?: ReducersMapObject<StateSchema, Action<any>> | undefined
 ) {
   const rootReducers: ReducersMapObject<StateSchema> = {
     ...asyncReducers,
@@ -18,10 +29,15 @@ export function createReduxStore (
 
   const reducerManager = createReducerManager(rootReducers)
 
-  const store = configureStore<StateSchema>({
-    reducer: reducerManager.reduce,
+  const store = configureStore({
+    reducer: reducerManager.reduce as Reducer<CombinedState<StateSchema>>,
     devTools: __IS_DEV__,
-    preloadedState: initialState
+    preloadedState: initialState,
+    middleware: getDefaultMiddleware => getDefaultMiddleware({
+      thunk: {
+        extraArgument: extraArg
+      }
+    })
   })
   // @ts-expect-error fix this code latter
   store.reducerManager = reducerManager
